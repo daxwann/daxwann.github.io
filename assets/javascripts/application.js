2b0188d3411af645d523cff5aee1f2cf1d31cbc0
Dax Wann <daxwann@gmail.com>
2019-09-27 01:27:51 -0700

    fix no scrolling on ios with ontouchend

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -2,15 +2,18 @@ function toggleNoScroll() {
   let checkbox = document.getElementById("navi-toggle");
   let body = document.querySelector("body");
   let html = document.querySelector("html");
+  let container = document.querySelector(".container");
 
   checkbox.addEventListener("click", (e) => {
     if (checkbox.checked) {
       html.classList.add("no-scroll")
       body.classList.add("no-scroll");
+      container.classList.add("no-scroll");
       body.addEventListener("touchmove", (e) => {
         e.preventDefault();
       }, false);
     } else {
+      container.classList.remove("no-scroll");
       body.classList.remove("no-scroll");
       html.classList.remove("no-scroll");
       body.removeEventListener("touchmove", (e) => {
