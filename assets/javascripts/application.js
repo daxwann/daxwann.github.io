74f0580a8971c8650794d039f38c418532e99cbf
Dax Wann <daxwann@gmail.com>
2019-09-27 00:52:09 -0700

    fix no scroll on mobile

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,12 +1,15 @@
 function toggleNoScroll() {
   let checkbox = document.getElementById("navi-toggle");
-  let body = document.getElementsByTagName("body")[0];
+  let body = document.querySelector("body");
+  let html = document.querySelector("html");
 
   checkbox.addEventListener("click", (e) => {
     if (checkbox.checked) {
+      html.classList.add("no-scroll")
       body.classList.add("no-scroll");
-    } else if (body.classList.contains("no-scroll")) {
+    } else {
       body.classList.remove("no-scroll");
+      html.classList.remove("no-scroll");
     }
   });
 }
